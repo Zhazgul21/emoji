2e8957dbcdea914033560d5909628316cf34436a
Zhazgul17 <azimova.zh17@gmail.com>
2023-03-31 18:13:27 +0300

    add

--- app.js
+++ app.js
@@ -1,30 +1,40 @@
-let card = {
-    emoji: '💯',
-    title: '100',
+import{data as emoji} from './data.js';
+
+
+ let card = {
+    title: '💯',
+    simbol: '100',
     keywords: 'Hundred, points, symbol, wow, win, perfect, parties',
 }
+function renderCard(data){
+    data.forEach(element => createCard(element));
 
+}
+renderCard(emoji)
 
 function createCard(obj) {
     const body = document.body;
+    const wrap = document.createElement('div')
+    wrap.className = 'wrapper'
     const cardDiv = document.createElement('div')
     cardDiv.className = 'card'
 
     const emoj = document.createElement('p')
     emoj.className = 'emoj'
-    emoj.innerText = card.emoji
+    emoj.innerText = card.title
 
     const title = document.createElement('p')
     title.className = 'title'
-    title.innerText = card.title
+    title.innerText = card.simbol
 
     const keywords = document.createElement('p')
     keywords.className = 'desc'
     keywords.innerText = card.keywords
 
 
+    wrap.append(cardDiv)
     cardDiv.append(emoj, title, keywords)
-    body.append(cardDiv)
+    body.append(wrap)
 
 }
 
